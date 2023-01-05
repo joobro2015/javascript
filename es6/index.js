bfb08be6b5a97cf9f512c8ce1159ed128ebb5a2f
joobro2015 <joobro2015@gmail.com>
2023-01-05 12:29:46 +0900

    study

--- es6/index.js
+++ es6/index.js
@@ -1,17 +1,114 @@
-{
+// {
+//     let kors = [10,20,30];
+//     let [kor1, kor2, kor3] = kors;
 
-    let attr = "kor";
-    let exam = {
+//     console.log(`kor1:${kor1}, kor2:${kor2}`)
 
-        [attr] : 10
+//     let temp = [40,70,80];
+//     [kor1, kor2, kor3] = temp;
 
-    }
+//     console.log(`kor1:${kor1}, kor2:${kor2}, kor3:${kor3}`)
+// }
+// {
+//     let kors = {
+//         kor1:10,
+//         kor2:20,
+//         kor3:30};
 
-    console.log(exam.kor);
-}
+
+//     console.log(kors.kor1)
+// }
+// {
+// let kors = [10, 20, 30];
+// let [kor1, kor2, kor3, kor4=40] = kors;
+
+// }
+
+let notices = {
+    title:"공지사항",
+    list:[
+        {title:"오~", content:"내용없을무"},
+        {title:"하하", content:"ㅋㅋㅋㅋ"}
+    ]  
+};
+
+let {list:[,notice]} = notices
+console.log(notice.title)
+
+
+// let exam = {
+//     kor : 90,
+//     math : 95,
+//      eng : 90,
+//      student : {
+//         name : "joobro",
+//         number : '2',
+//         phone : "s21"
+//      }
+//  };
+//  let {kor: korean} = exam;
+//  console.log(korean)
+//  let{eng: english} = exam;
+//  console.log(english)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+
+//     let attr = "kor";
+//     let exam = {
+
+//         [attr] : 10
+
+//     }
+
+//     console.log(exam.kor);
+// }
+
+// let exam = {
+//     kor : 90,
+//     math : 95,
+//      eng : 90
+//  }
+
+
+// function print({kor,math,eng}){
+
+//     console.log(`kor:${kor},math:${math},eng:${eng}`)
+
+// }
+
+
+// let{kor: hangle, student:{phone,name,number}} = exam;
+// console.log(`kor:${hangle}, phone:${phone}, name:${name}, number:${number}`)
+
+
+
+// {
+// function printExam(exam){
+// var kor = exam.kor;
+// var eng = exam.eng;
+// var math = exam.math;
+
+// var total = kor + eng + math;
+
+// console.log(`kor : ${kor}, eng : ${eng}, math:${math}`);
+// console.log(`total is ${total}`)
+
+// }
+// }
 
 
 // {
