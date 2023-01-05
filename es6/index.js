49a85ee75be42c89c0b3ef65b49113c8e7da79a6
joobro2015 <joobro2015@gmail.com>
2023-01-05 11:17:50 +0900

    es6 study

---
+++ es6/index.js
@@ -0,0 +1,59 @@
+{
+
+    let attr = "kor";
+    let exam = {
+
+        [attr] : 10
+
+    }
+
+    console.log(exam.kor);
+}
+
+
+
+
+
+// {
+//     let title = "ES6"
+//     let content = "새로운 문자열"
+//     let template = String.raw`<section>
+//                         <h1>${title}</h1>
+//                         <p>${content}</p>
+//                     </section>`;
+
+//     console.log(template);
+// }
+// {
+//     let kor =70;
+//     let eng =80;
+//     let math = 100;
+
+//     let exam = {kor,eng,math,total(){
+//         return kor+eng+math
+
+//     }};
+//     console.log(exam.total);
+// }
+
+
+// -----const----------
+// {
+//     const N = 1;
+//     const S = 5;
+
+//     var walkTo = 'S';
+// }
+// // S = 3;
+
+// {
+//     var add = function (a, b) {
+//         return a + b;
+//     }
+
+//     console.log(add(3, 4));
+// }
+
+// ----------------------------------
+// let x = 30;
+// console.log(x);
